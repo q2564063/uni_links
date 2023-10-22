eff88898ddf7a074696e1a1aad4942c3922f4878
zzqs-mac-mini <597238072@qq.com>
2023-10-22 12:02:58 +0800

    first commit

---
+++ uni_links/ios/Classes/UniLinksPlugin.h
@@ -0,0 +1,14 @@
+#import <Flutter/Flutter.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UniLinksPlugin : NSObject <FlutterPlugin>
++ (instancetype)sharedInstance;
+- (BOOL)application:(UIApplication *)application
+    continueUserActivity:(NSUserActivity *)userActivity
+      restorationHandler:(void (^)(NSArray *_Nullable))restorationHandler;
+- (void)resetInitialLink:(NSString *)initialLink;
+- (void)resetLatestLink:(NSString *)latestLink;
+@end
+
+NS_ASSUME_NONNULL_END
